3620e6c57147792c0bd9b7efb5721db574ef16ee
Acacia Ackles <alackles@msu.edu>
2021-09-10 12:37:34 -0400

    Write out knockouts file

--- source/evaluate/static/EvalNKRank.hpp
+++ source/evaluate/static/EvalNKRank.hpp
@@ -27,17 +27,19 @@ namespace mabe {
 
     std::string bits_trait;
     std::string fitness_trait;
+    std::string knockout_file;
 
   public:
     EvalNKRank(mabe::MABE & control,
            const std::string & name="EvalNKRank",
            const std::string & desc="Module to evaluate bitstrings on an NK Fitness Lanscape WITH rank epistasis baked in.",
-           size_t _N=100, size_t _K=3, const std::string & _btrait="bits", const std::string & _ftrait="fitness")
+           size_t _N=100, size_t _K=3, const std::string & _btrait="bits", const std::string & _ftrait="fitness", const std::string & _kfile="knockouts.csv")
       : Module(control, name, desc)
       , N(_N), K(_K)
       , target_collect(control.GetPopulation(0))
       , bits_trait(_btrait)
       , fitness_trait(_ftrait)
+      , knockout_file(_kfile)
     {
       SetEvaluateMod(true);
     }
@@ -49,6 +51,7 @@ namespace mabe {
       LinkVar(K, "K", "Number of bits used in each gene");
       LinkVar(bits_trait, "bits_trait", "Which trait stores the bit sequence to evaluate?");
       LinkVar(fitness_trait, "fitness_trait", "Which trait should we store NK fitness in?");
+      LinkVar(knockout_file, "knockout_file", "Where should we save the information about knockouts?");
     }
 
     void SetupModule() override {
@@ -91,17 +94,28 @@ namespace mabe {
     // Rank epistasis analysis on the final population
     void BeforeExit() override {
       mabe::Collection alive_collect( target_collect.GetAlive() );
+      std::ofstream kfileout(knockout_file);
+      kfileout << "org_ID,pos,score_WT,score_KO,\n";
+      int org_id = 0;
       for (Organism & org : alive_collect) {
         org.GenerateOutput();
         const auto & bits = org.GetTrait<emp::BitVector>(bits_trait);
+        double wt_fitness = landscape.GetFitness(bits);
         auto knockout = bits;
         for (int i = 0 ; i < N ; ++i) {
           knockout.Toggle(i);
           int ko_pos = i;
           double ko_fitness = landscape.GetFitness(knockout);
           knockout.Toggle(i);
+          kfileout << org_id << ",";
+          kfileout << ko_pos << ",";
+          kfileout << wt_fitness << ",";
+          kfileout << ko_fitness << ",";
+          kfileout << "\n";
         }
+        org_id++;
       }
+      kfileout.close();
     }
       
   };
