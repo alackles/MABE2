55a4f439add5dfae645922b6d991016c4f27043f
Acacia Ackles <alackles@msu.edu>
2021-10-13 11:56:28 -0400

    Update to have mixed K

--- source/evaluate/static/EvalNKRank.hpp
+++ source/evaluate/static/EvalNKRank.hpp
@@ -21,10 +21,14 @@ namespace mabe {
   class EvalNKRank : public Module {
   private:
     size_t N;
-    size_t K;    
-    NKLandscape landscape;
+    size_t K;
+    size_t K_alt;    
+    NKLandscape landscape_full;
+    NKLandscape landscape_a;
+    NKLandscape landscape_b;
     mabe::Collection target_collect;
 
+    std::string nk_type;
     std::string bits_trait;
     std::string fitness_trait;
     std::string mutant_file;
@@ -35,12 +39,14 @@ namespace mabe {
     EvalNKRank(mabe::MABE & control,
            const std::string & name="EvalNKRank",
            const std::string & desc="Module to evaluate bitstrings on an NK Fitness Lanscape WITH rank epistasis baked in.",
-           size_t _N=100, size_t _K=3, 
+           size_t _N=100, size_t _K=3, size_t _Kalt=0,
+           const std::string & _nktype="standard",
            const std::string & _btrait="bits", const std::string & _ftrait="fitness", 
            const std::string & _mfile="mutants.csv", const std::string & _nkfile="nk.csv", const std::string & _gfile="ref_genome.csv")
       : Module(control, name, desc)
-      , N(_N), K(_K)
+      , N(_N), K(_K), K_alt(_Kalt)
       , target_collect(control.GetPopulation(0))
+      , nk_type(_nktype)
       , bits_trait(_btrait)
       , fitness_trait(_ftrait)
       , mutant_file(_mfile)
@@ -55,6 +61,8 @@ namespace mabe {
       LinkCollection(target_collect, "target", "Which population(s) should we evaluate?");
       LinkVar(N, "N", "Number of bits required in output");
       LinkVar(K, "K", "Number of bits used in each gene");
+      LinkVar(K_alt, "K_alt", "Number of bits used in each gene for the second landscape. If only one landscape (i.e. nk_type is standard), this is ignored.");
+      LinkVar(nk_type, "nk_type", "Type of NK landscape to evaluate on. [standard = canonical Kauffman, half = split in half, mixed = intermixed]")
       LinkVar(bits_trait, "bits_trait", "Which trait stores the bit sequence to evaluate?");
       LinkVar(fitness_trait, "fitness_trait", "Which trait should we store NK fitness in?");
       LinkVar(mutant_file, "mutant_file", "Where should we save the information about mutants?");
@@ -68,10 +76,16 @@ namespace mabe {
       AddOwnedTrait<double>(fitness_trait, "NK fitness value", 0.0);
 
       // Setup the fitness landscape.
-      landscape.Config(N, K, control.GetRandom());  // Setup the fitness landscape.
+      if (nk_type == "half" || nk_type == "mixed") {
+        size_t midpt = N/2;
+        landscape_a.Config(midpt, K, control.GetRandom());
+        landscape_b.Config(midpt, K_alt, control.GetRandom());
+      } else {
+        landscape_full.Config(N, K, control.GetRandom());  // Setup the fitness landscape.
+        PrintLandscape(landscape);
+      }
     
       // Output the fitness landscape.
-      PrintLandscape(landscape);
     
     }
 
@@ -105,12 +119,18 @@ namespace mabe {
       for (Organism & org : alive_collect) {
         org.GenerateOutput();
         const auto & bits = org.GetTrait<emp::BitVector>(bits_trait);
-        if (bits.size() != N) {
+        double fitness = 0;
+        if (nk_type == "half") {
+          double fitness_a = landscape.GetFitness({bits.begin(), bits.begin() + N/2});
+          double fitness_b = landscape.GetFitness({bits.begin() + N/2, bits.end()});
+          fitness = fitness_a + fitness_b; 
+        } else {
+          fitness = landscape.GetFitness(bits);
+          if (bits.size() != N) {
           AddError("Org returns ", bits.size(), " bits, but ",
                    N, " bits needed for NK landscape.",
                    "\nOrg: ", org.ToString());
         }
-        double fitness = landscape.GetFitness(bits);
         org.SetTrait<double>(fitness_trait, fitness);
 
         if (fitness > max_fitness || !max_org) {
